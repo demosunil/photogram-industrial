9cf977a123d5b7a76d78a37a21cf3221a1d08ec5
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-05-19 03:44:20 +0000

    assignment completed

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
   #resources :users, only: :show
-  get "/:username" => "users#show"
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  #get ":username/feed" => "photos#liked", as: :liked_photos
+  #get ":username/followers" => "photos#liked", as: :liked_photos
+  #get ":username/following" => "photos#liked", as: :liked_photos
+
+
+  get "/:username" => "users#show", as: :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
